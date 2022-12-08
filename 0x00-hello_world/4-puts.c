14240635c47bf098bc36a008197ed92227009972
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-08 13:25:48 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main-empty
+ * main - main input
  * return: o
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
