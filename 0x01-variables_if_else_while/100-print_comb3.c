577aaa7061923e451934672f37023cad1ad8476a
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-09 17:30:42 -0800

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* main - Prints all possible different combinations of two digits
-*
-* Return: Always 0
+* main - main block
+* Description: Write a program that prints all possible
+* different combinations of two digits
+* Numbers must be separated by ,, followed by a space
+* The two digits must be different
+* 01 and 10 are considered the same combination of the two digits 0 and 1
+* Print only the smallest combination of two digits
+* Numbers should be printed in ascending order, with two digits
+* You can only use the putchar function
+* (every other function (printf, puts, etc…) is forbidden)
+* You can only use putchar five times maximum in your code
+* You are not allowed to use any variable of type char
+* All your code should be in the main function
+* Return: 0
 */
 int main(void)
 {
-int ones = '0';
-int tens = '0';
-for (tens = '0'; tens <= '9'; tens++)
+int c;
+int d;
+int e = 0;
+while (e < 10)
 {
-for (ones = '0'; ones <= '9'; ones++)
+d = 0;
+while (d < 10)
 {
-if (!((ones == tens) || (tens < ones)))
+c = 0;
+while (c < 10)
 {
-putchar(tens);
-putchar(ones);
-if (!(ones == '9' && tens == '8'))
+if (c != d && d != e && e < d && d < c)
+{
+putchar('0' + e);
+putchar('0' + d);
+putchar('0' + c);
+if (c + d + e != 9 + 8 + 7)
 {
 putchar(',');
 putchar(' ');
 }
 }
+c++;
 }
+d++;
+}
+e++;
 }
 putchar('\n');
 return (0);
