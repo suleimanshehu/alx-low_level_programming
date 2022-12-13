a74f26f513b169b38c64861c6481088b9f44d5fc
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-13 15:35:16 -0800

    Self

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet_x10-printallalphabetx10
+*/
+void print_alphabet_x10(void)
+{
+char i, letters;
+
+for (i = '0'; i <= '9';i++)
+{
+for (letters = 'a'; letters <= 'z'; letters++)
+{
+_putchar(letters);
+}
+_putchar('\n');
+}
