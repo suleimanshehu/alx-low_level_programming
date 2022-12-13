6081fab08ca4f2fd0f926a88dd24139172c4dde6
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-13 15:15:10 -0800

    Main c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+*print_alphabet-printallalphabetinlowercase
+*/
+void print_alphabet(void)
+{
+char(letters);
+for(letters='a';letters<='z';letters++)
+_putchar(letters);
+_putchar('\n');
+}
