c0736e13d182e5a37ad88b11dcb03fa7155a7787
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-13 14:30:21 -0800

    Hello 0-putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+/**
+** _putchar - writes the character ctostdout
+** @c:the character to print
+** **Return:on success1.
+** On error,-1 is returned, and error is set appropriately.
+*
+*
+**
+*/
+int _putchar(char c)
+{
+return (write(1, &c,1));
+}
