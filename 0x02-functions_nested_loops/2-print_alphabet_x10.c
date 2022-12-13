35dd0f647eae79b7533fa76e1b6a19011375897f
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-13 15:59:02 -0800

    Hello

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,9 @@ void print_alphabet_x10(void)
 {
 char i, letters;
 
-for(i = '0'; i <= '9'; i++)
+for (i = '0'; i <= '9'; i++)
 {
-for(letters = 'a'; letters <= 'z'; letters++)
+for (letters = 'a'; letters <= 'z'; letters++)
 {
 _putchar(letters);
 }
