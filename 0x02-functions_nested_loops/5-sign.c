13b0f8179f4d08be9810ee87474bd02a2ca179ab
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-14 14:31:39 -0800

    Al

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,17 +7,19 @@
 *-1ifnumberislesthanzero
 */
 int print_sign(int n)
-{if (n > 0)
 {
-_putchar('+');
+if (n > 0)
+{
+_putchar ('+');
 return (1);
 }
 else if (n == 0)
 {
-_putchar('0');
+_putchar ('0');
 return (0);
 }
 else
 {
 _putchar('-');
+}
 }
