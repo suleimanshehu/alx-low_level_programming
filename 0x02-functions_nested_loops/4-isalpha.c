8838fbf5524baa71aa0bd172d0c5d1f3c0819097
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-13 16:15:04 -0800

    khesh

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isalpha-checksforalphabet
+*@c:charactertobechecked
+*Return:1ifcharacterisaletter,0ifotherwise
+*/
+int _isalpha(int c)
+{
+if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
