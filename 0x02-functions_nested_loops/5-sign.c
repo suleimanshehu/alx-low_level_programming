c9a1c5b51139df9023d144b2243315ada145cef9
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-13 16:22:37 -0800

    sign sh

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_sign-printsthesignsofanumber
+*@n:thenumberofsignstobeprinted
+*Return:1ifnumberisgreaterthanzero
+*0ifnumberiszero
+*-1ifnumberislesthanzero
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+elseif (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
