67aaba1bbab53c513894c772b00c7dbdace51f64
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-15 12:45:29 -0800

    Hello

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
 /**
 *  * more_numbers - prints 10 times the number
-*   * from 0 to 14
-*    * Return: no return
-*     */
+*  * from 0 to 14
+*  * Return: no return
+*
+*/
 void more_numbers(void)
 {
 int num1, num2;
-
 for (num1 = 0; num1 < 10; num1++)
 {
 for (num2 = 0; num2 < 15; num2++)
@@ -17,5 +16,6 @@ if (num2 > 9)
 _putchar((num2 / 10) + '0');
 _putchar((num2 % 10) + '0');
 }
-_putchar('\n');											}
+ _putchar('\n');
+}
 }
