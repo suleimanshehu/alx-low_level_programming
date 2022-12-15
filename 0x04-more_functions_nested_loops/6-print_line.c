32f6add4402cc20df7e8d3d4c95612c120614028
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2022-12-15 13:36:20 -0800

    hello

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,30 +1,11 @@
-
 #include "main.h"
-
 /**
- * print_line - draws a straight line
- * @n: number of tines _ should be printed
- */
-void print_line(int n)
-{
-  int i;
-
-  for (i = 1; i <= n; i++)
-    {
-      _putchar('_');
-    }
-  _putchar('\n');
-}
-#include "main.h"
-
-/**
- * print_line - draws a straight line
- * @n: number of tines _ should be printed
- */
+* print_line - draws a straight line
+* @n: number of tines _ should be printed
+*/
 void print_line(int n)
 {
 int i;
-
 for (i = 1; i <= n; i++)
 {
 _putchar('_');
