f7aab1b1c5610f06252cb99eba008f53702f709f
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2023-01-02 16:30:56 -0800

    Pointer event

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,9 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
+/**
+ * File: main.h
+ * Auth: Shehu
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
 int _putchar(char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
+void *_memset(void *s, int c, size_t n);
+void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
@@ -11,4 +20,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-#
+
+#endif
