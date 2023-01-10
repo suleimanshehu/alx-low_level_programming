b58c68aa1f7a4d4c316d363c8fd9c41d1f37642b
SUELIMAN TIJJANI SHEHU <suleimanshehutijjani@gmail.com>
2023-01-10 07:30:02 -0800

    Frge

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* _isupper - checks if parameter is an uppercase character.
-* @c: input character.
-* Return: 1 if it is an uppercase character, 0 in the other case.
-*/
+* * _isupper - checks if parameter is an uppercase character.
+* * @c: input character.
+* * Return: 1 if it is an uppercase character, 0 in the other case.
+* */
 
 int _isupper(int c)
 {
