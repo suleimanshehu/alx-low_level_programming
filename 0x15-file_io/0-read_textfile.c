d86d338ca490d16914cd13e24cfbfa444204a650
Suleiman Tijjani Shehu <suleimanshehutijjani@gmail.com>
2023-02-06 14:12:52 -0800

    File o/i

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,37 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
 #include "main.h"
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-int fd;
-ssize_t nr, nw;
-char buffer[letters];
+/**
+* read_textfile - reads a file and writes it to stdout
+* @filename: name of the file to read
+* @letters: number of characters to read/write
+* Return: number of characters read/written
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t read_count, write_count, fd;
+char *buffer;
 
-if (!filename)
-return 0;
-
+if (!filename || letters == 0)
+return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
-return 0;
-
-nr = read(fd, buffer, letters);
-if (nr == -1)
-return 0;
-
-nw = write(STDOUT_FILENO, buffer, nr);
-if (nw == -1 || nw != nr)
-return 0;
-
+return (0);
+buffer = malloc(letters);
+if (buffer == NULL)
+{
 close(fd);
-return nw;
+return (0);
+}
+read_count = read(fd, buffer, letters);
+close(fd);
+if (read_count <= 0)
+{
+free(buffer);
+return (0);
+}
+write_count = write(STDOUT_FILENO, buffer, read_count);
+free(buffer);
+if (write_count < 0)
+return (0);
+return (write_count);
 }
